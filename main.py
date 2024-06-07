f5fc812cb0d650d69b07fae05a632caf59d9db27
Rostyslav Hundarev <r.hundarev.im21@kpi.ua>
2024-06-07 14:28:39 +0300

    Create starting bot

---
+++ main.py
@@ -0,0 +1,28 @@
+import asyncio
+import logging
+
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+
+import json
+
+with open('config.json') as config_file:
+    config = json.load(config_file)
+
+API_Token = config['API_Token']
+
+
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def starting(message: types.Message):
+    await message.answer(text="Вас вітає Rostyslav-GPT! 🖖")
+
+async def main():
+    logging.basicConfig(level=logging.INFO)
+    bot = Bot(token=API_Token)
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
