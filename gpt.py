fbb41f75899cdf3ffeb98f7599233ddc1b716a26
Rostyslav Hundarev <r.hundarev.im21@kpi.ua>
2024-06-10 18:58:42 +0300

    add gpt answering

--- gpt.py
+++ gpt.py
@@ -8,12 +8,28 @@ client = OpenAI(
     api_key= config['GPT_API_TOKEN']
 )
 
-completion = client.chat.completions.create(
-  model="gpt-3.5-turbo",
-  messages=[
-    {"role": "system", "content": "You are just a companion who can always help, always answering correctly and without long texts."},
-    {"role": "user", "content": "Hi!"}
-  ]
-)
+def read_last_message():
+    try:
+        with open("last_message.txt", "r") as file:
+            return file.read().strip()
+    except FileNotFoundError:
+        return None
+    
+def save_answer_message(answer):
+    with open("answer_message.txt", "w") as file:
+        file.write(answer)
 
-print(completion.choices[0].message)
+last_message = read_last_message()
+if last_message:
+  completion = client.chat.completions.create(
+    model="gpt-3.5-turbo",
+    messages=[
+      {"role": "system", "content": "You are just a companion who can always help, always answering correctly and without long texts."},
+      {"role": "user", "content": read_last_message()}
+    ]
+  )
+  generated_text = completion.choices[0].message.content
+  save_answer_message(generated_text)
+  print(generated_text)
+else:
+    print("No last message found.")
--- main.py
+++ main.py
@@ -1,28 +1,47 @@
 import asyncio
 import logging
-
+import os
 from aiogram import Bot, Dispatcher, types
 from aiogram.filters import CommandStart
-
-
 import json
+import time
 
 with open('config.json') as config_file:
     config = json.load(config_file)
 
 API_TOKEN = config['BOT_API_TOKEN']
 
-
 dp = Dispatcher()
+
+def save_last_message(message):
+    with open("last_message.txt", "w") as file:
+        file.write(message)
+
+def read_answer_message():
+    with open("answer_message.txt", "r") as file:
+        return file.read().strip()
 
 @dp.message(CommandStart())
 async def starting(message: types.Message):
     await message.answer(text="Вас вітає Rostyslav-GPT! 🖖")
 
+@dp.message()
+async def answer_message(message: types.Message):
+    user_message = message.text
+    logging.info(F" Received message: '{user_message}', from user: {message.from_user.id}")
+    save_last_message(user_message)
+
+    os.system("py gpt.py")
+    while not os.path.exists("answer_message.txt"):
+        time.sleep(0.1)
+
+    gpt_answer = read_answer_message()
+    logging.info(F" Replied: '{gpt_answer}', to user: {message.from_user.id}")
+    await message.reply(text=gpt_answer)
+
 async def main():
     logging.basicConfig(level=logging.INFO)
     bot = Bot(token=API_TOKEN)
-    await bot.delete_webhook(drop_pending_updates=True)
     await dp.start_polling(bot)
 
 if __name__ == "__main__":
